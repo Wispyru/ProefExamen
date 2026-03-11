8ffd6e2feff1a7b6f16621302c751e156f281a5a
Wispy <114589809+Wispyru@users.noreply.github.com>
2026-03-11 13:50:20 +0100

    added the customers

--- DinerDash/Assets/Scripts/CustomerScript/CustomerSpawning/CustomerGeneration.cs
+++ DinerDash/Assets/Scripts/CustomerScript/CustomerSpawning/CustomerGeneration.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CustomerGeneration : MonoBehaviour
@@ -6,11 +7,15 @@ public class CustomerGeneration : MonoBehaviour
     // serialized variables
     [SerializeField]
     private GameObject _customersToSpawn;
+    [SerializeField]
+    private GameObject[] _customerList;
 
     // private variables
     private Transform[] _customerSlot;
     private int _minCustomers = 0;
     private int _maxCustomers = 4;
+    private int _customerSelectionIndex;
+    private List<int> selectedCustomers;
 
     // public variables
     public int SpawnedCustomerAmount;
@@ -18,6 +23,7 @@ public class CustomerGeneration : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        selectedCustomers = new List<int>();
         SpawnedCustomerAmount = Random.Range(_minCustomers, _maxCustomers);
         _customerSlot = new Transform[gameObject.transform.childCount];
         for (int i = 0; i < _customerSlot.Length; i++)
@@ -33,7 +39,18 @@ public class CustomerGeneration : MonoBehaviour
         for (int i = 0; i <= SpawnedCustomerAmount; i++)
         {
             Debug.Log("Spawn Customer");
+            
+            SelectCustomer();
             Instantiate(_customersToSpawn, _customerSlot[i].position, _customerSlot[i].rotation, _customerSlot[i]);
         }
+    }
+
+    private void SelectCustomer()
+    {
+        _customerSelectionIndex = Random.Range(0, 3);
+        if (selectedCustomers.Contains(_customerSelectionIndex)) return;
+        
+        selectedCustomers.Add(_customerSelectionIndex); 
+        _customersToSpawn = _customerList[_customerSelectionIndex];
     }
 }
